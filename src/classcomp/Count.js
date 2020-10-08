97fb9e70b93f35d9833f0b60e554ff476b99fccb
joeybodoia <53023300+joeybodoia@users.noreply.github.com>
2020-10-07 18:22:09 -0700

    Initial commit

---
+++ src/classcomp/Count.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Count extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      count: this.props.start
+    }
+  }
+
+
+  addOne(){
+  this.setState({count:this.state.count+1})
+  console.log(this.state.count)
+  }
+
+  render() {
+    const above = <h1> It's 10 or above </h1>
+    const below = <h1> It's below 10 </h1>
+
+    return (
+    <>
+      {this.state.count < 10 ? below : above}
+      <h1>{this.state.count}</h1>
+      <button onClick={()=>this.addOne()}>Add One</button>
+    </>
+    )
+  }
+}
+
+export default Count;
---
+++ src/funccomp/Count.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const Count = (props) => {
+
+  const [count,setCount] = React.useState(props.start)
+
+  const addOne = ()=>{
+    setCount(count+1)
+  }
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={addOne}>Add One</button>
+    </>
+  )
+}
+
+export default Count
